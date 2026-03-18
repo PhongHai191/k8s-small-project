bb9ee0724ab7f2e02066b24460e16e2175b6239c
PhongHai191 <phonghai1901@gmail.com>
2026-03-18 17:20:41 +0700

    Add files via upload

---
+++ small_project/webapp/backend/metrics.js
@@ -0,0 +1,12 @@
+const client = require("prom-client")
+
+// system metrics (CPU, RAM...)
+client.collectDefaultMetrics()
+
+const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "HTTP request duration",
+  labelNames: ["method", "route", "status"],
+})
+
+module.exports = { client, httpRequestDuration }
